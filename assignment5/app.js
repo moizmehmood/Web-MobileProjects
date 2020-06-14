b261267ad02c356de076b9e6c0bc767b6dfbd3e8
moiz mehmood <moizmehmood212@gmail.com>
2020-06-14 19:41:49 +0500

    javascrept assignment

---
+++ assignment5/app.js
@@ -0,0 +1,62 @@
+////////// chapter 1 ////////////
+////////////task 1//////////////
+// alert("Wellcome");
+
+////////////task 2//////////////
+// alert("Error! please enter a valid password");
+
+////////////task 3//////////////
+// document.write("Welcome to js lang..." + "<br>"+"happy coding");
+
+////////////task 4//////////////
+// alert("welcome to JS language");
+// alert("prevent this page from creating additional dialogs")
+
+////////////task 5//////////////
+// console.log("hellow i can run js through my web  browser console")
+
+
+////////////////// chapter 2 /////////////////
+/////////// task 1 ///////////////
+// var username;
+
+////////////task 2//////////////
+// var myName = "moz mehmood";
+
+////////////task 3//////////////
+// var a="hellow world";
+// alert(a);
+
+////////////task 4//////////////
+// var name = "moiz mehmood";
+// var age = 23;
+// var course = "web and mobile development"
+
+// alert(name);
+// alert(age);
+// alert(course);
+
+
+////////////task 6//////////////
+
+// var email = "moizmehmood212@gmail.com";
+// alert("my email address is "+email)
+
+///////////// task 7 /////////
+// var book = "A smarter way to learn JavaScript";
+// alert("i am trying to earn from the book  "+book)
+
+
+////////////// task 8 //////////
+// console.log("yah i can learn JS")
+
+
+
+
+//////////////////chapter 3 //////////
+
+//////////// task 1//////////////////
+// var age = 15
+// alert("i am "+age +" years old");
+
+
