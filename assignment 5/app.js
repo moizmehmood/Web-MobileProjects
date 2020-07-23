6046c47d04d209616b9d3f1b0e71a1cd9717e2b6
moiz mehmood <moizmehmood212@gmail.com>
2020-07-23 22:13:50 +0500

    assignment 5

---
+++ assignment 5/app.js
@@ -0,0 +1,296 @@
+////////// chapter 1 ////////////
+////////////task 1//////////////
+// alert("Wellcome");
+
+////////////task 2//////////////
+// alert("Error! please enter a valid password");
+
+////////////task 3//////////////
+// document.write("Welcome to js lang..." + "<br>"+"happy coding");
+
+////////////task 4//////////////
+// alert("welcome to JS language");
+// alert("prevent this page from creating additional dialogs")
+
+////////////task 5//////////////
+// console.log("hellow i can run js through my web  browser console")
+
+////////////task 6//////////////
+// alert("welcome to assignment 5")
+
+
+////////////////////////////////////////////
+////////////////// chapter 2 /////////////////
+///////////////////////////////////////////
+
+/////////// task 1 ///////////////
+// var username;
+
+////////////task 2//////////////
+// var myName = "moz mehmood";
+
+////////////task 3//////////////
+// var massage="hellow world";
+// alert(massage);
+
+////////////task 4//////////////
+// var name = "moiz mehmood";
+// var age = 23;
+// var course = "web and mobile development"
+
+// alert(name);
+// alert(age);
+// alert(course);
+
+/////////////////task 5 /////////////////
+// document.write("PIZZA" + "<br>"+"PIZZ" + "<br>" + "PIZ" + "<br>" + "PI" + "<br>" + "P");
+
+////////////task 6//////////////
+
+// var email = "moizmehmood212@gmail.com";
+// alert("my email address is "+email)
+
+///////////// task 7 /////////
+// var book = "A smarter way to learn JavaScript";
+// alert("i am trying to earn from the book  "+book)
+
+
+////////////// task 8 //////////
+// console.log("yah i can learn JS")
+
+///////////////////TASK 9 ////////
+
+// var a = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert(a)
+
+
+//////////////////////////////////////
+//////////////////chapter 3 //////////
+//////////////////////////////////////
+
+//////////// task 1//////////////////
+// var age = 15
+// alert("i am "+age +" years old");
+
+/////////////task 2 ////////////
+
+////////////////task 3 /////////////
+// var birth_year = 1997
+// document.write("my date or birth is 1997" + "<br>" + "data type of my declared vartiable is number")
+
+////////////////// task 4 ////////////////
+
+// var name = prompt("full name")
+// var order = prompt("number of shir")
+// // alert(name)
+// document.write(" " +name + " ordered " +order + " T-shirt on XYZ Clothing store")
+
+
+//////////////////////////////////////
+//////////////////chapter 4 //////////
+//////////////////////////////////////
+
+
+//////////////////////////////////////
+//////////////////chapter 5 //////////
+//////////////////////////////////////
+
+///////////////////task 1 /////////////////
+
+// var sum1=+prompt("enter 1st number")
+// var sum2=+prompt("enter 2nd number")
+// var sum= sum1 + sum2;
+// document.write("sum of "+sum1 + " and "+sum2 + " is "+sum)
+
+///////////////////task 2 //////////////////
+
+// var number1=+prompt("enter 1st number")
+// var number2=+prompt("enter 2nd number")
+// var sub= number1 - number2;
+// document.write("sub of "+number1 + " and "+number2 + " is "+sub)
+
+// var number1=+prompt("enter 1st number")
+// var number2=+prompt("enter 2nd number")
+// var multy= number1 * number2;
+// document.write("multiplay of "+number1 + " and "+number2 + " is "+multy)
+
+/////////////task 3/////////////////
+// var initialValue=+prompt("enter initial value")
+// document.write("inital value "+initialValue + "<br>")
+// var increrment=initialValue + 1;
+// document.write("value after increment "+increrment + "<br>")
+// var addition= +prompt("enter the value of addition")
+// var additionValue=increrment+addition;
+// document.write("value after addition "+additionValue + "<br>")
+// var decrement=additionValue - 1;
+// document.write("value after decrement "+decrement + "<br>"
+// )
+
+//////////task 4////////////////
+
+// var movieTicket= 600;
+// var totalTicket=+prompt("enter number of buy ticket")
+// var total= +totalTicket * +movieTicket;
+// document.write("total cost to buy " +totalTicket + " ticket to a movie is " +total );
+
+/////////////////task 5 /////////////////
+
+// var tableNo = +prompt("table of")
+//  document.write("table of "+tableNo+"<br>")
+// var i;
+// for (i = 1; i <= 10; i++){
+// var table = i * tableNo;
+// document.write(" "+tableNo +" x "+i +" = "+ table +"<br>")
+// }
+
+//////////////// task 7 //////////////
+
+// var item1= +prompt("enter the quantity of item 1")
+// var item2=+prompt("enter the quantity of item 2")
+// document.write("price of item 1 is 650 <br>")
+// document.write("quantity of item 1 is " +item1 + "<br>")
+// document.write("price of item 2 is 100<br>")
+// document.write("quantity of item 2 is " +item2 + "<br>")
+// document.write("shipping cgarges 100 <br>")
+
+// var total1=item1 * 650;
+// var total2=item2 * 100;
+// var total=total1 + total2 + 100;
+// document.write("total cost of your order is " +total + "<br>")
+
+//////////////////task 8 /////////////////
+
+// var totalMarks=+prompt("enter total marks ")
+// var obtainMarks=+prompt("enter obtain  marks ")
+
+// var percentage =(obtainMarks * 100) / totalMarks;
+// document.write("total marks " + totalMarks + "<br> marks obtain " + obtainMarks  + "<br> percentage "+percentage)
+
+//////////////// task 9 ////////////////
+
+// var usDollar= +prompt("enter dallor ammount ")
+// var saudiReyal=28 *usDollar;
+// var pakistanRupes= 104 * usDollar;
+
+// document.write("the "+usDollar+" US dallary =  "+saudiReyal+" saudia riyal <br>5")
+// document.write("the "+usDollar+" US dallary =  "+pakistanRupes+" Pakistan Rupess")
+
+
+///////////// task 10 ///////////////////////
+
+// var number = +prompt("enter number")
+// var add = number + 5;
+// var multy = number * 10;
+// var devide = number / 2;
+
+// document.write("enter number is :  " + number + "<br>add 5 than number is  "
+//  + add + "<br>multiplay by 10 than number is : " + multy + "<br>divided by 2 than number is : " + devide)
+
+//////////////// task 11 //////////////////
+// var birthYear=+prompt("enter birth year")
+// var currentYear= +prompt("enter current year")
+
+// var age=currentYear-birthYear;
+// document.write("current year : "+currentYear+"<br>birth year : "+birthYear+"<br>your age is : "+age)
+
+
+//////////////// task 12 ////////////////////////
+
+// var radiusCircle=+prompt("enter the radius of circile")
+// var circumference=2*3.142*radiusCircle;
+// var area=3.142*radiusCircle*radiusCircle;
+
+// document.write("radius of circle is  : "+radiusCircle+"<br>the cercumference is : "+circumference+"<br>the area is : "+area)
+
+
+
+///////////////////////////////////////////
+//////////////////chapter 6 to 9 //////////
+///////////////////////////////////////////
+
+
+///////////////// task 1 //////////////////
+
+// var a=+prompt("enter value")
+// document.write("the value of a is : "+a+"<br>------------------------<br><br>the value ++a is : "
+// + ++a+"<br> now tha  value of a is : "+a+"<br><br>the value of a++ is : "+ a++ 
+// +"<br>now thw value of a is : "+a+"<br><br>the value of --a is :"+ --a +"<br>now the value of a is : "
+// +a+"<br><br>the value of a-- is : "+ a-- +"<br>now thw value of a is : "+a)
+
+
+////////////////// task 2 ///////////////
+ /// same as task 1 
+
+////////////////////task 3 //////////////
+
+// var name=prompt("enter name")
+// document.write("welcome "+name)
+
+//////////////////////task 6 //////////////
+
+// var english=+prompt("enter the marks of english")
+// var math=+prompt("enter the marks of math")
+// var urdu=+prompt("enter the marks of urdu")
+// var totalSubjectMarks = 100;
+// var engpercentage =(english * 100) / 100;
+// var mathpercentage =(math * 100) / 100
+// var urdupercentage =(urdu * 100) / 100
+// var totalNo=english+urdu+math;
+// var totalPercentage=(totalNo * 100) / 300;
+// document.write("subject"+"\xa0\xa0\xa0\xa0\xa0\xa0\xa0"+ "total marks" + "\xa0\xa0\xa0\xa0\xa0\xa0\xa0"+  "obtain marks"+ "\xa0\xa0\xa0\xa0\xa0\xa0\xa0"+ "percentage<br>")
+
+// document.write("english"+"\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0"+ "100" + "\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0"+  " "+english+ "\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0"+ " "+engpercentage+"%<br>")
+
+// document.write("urdu  "+"\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0"+ "100" + "\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0"+  " "+urdu+ "\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0"+ " "+urdupercentage+"%<br>")
+
+// document.write("math"+"\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0"+ "100" + "\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0"+  " "+math+ "\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0"+ " "+mathpercentage+"%<br>")
+
+// document.write("total"+"\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0"+ "300" + "\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0"+" "+totalNo+ "\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0"+ " "+engpercentage+"%<br>")
+
+
+///////////////////////////////////////////
+//////////////////chapter 9 to 11 //////////
+///////////////////////////////////////////
+
+
+
+
+///////////////////////////////////////////
+//////////////////chapter 14 to 16 //////////
+///////////////////////////////////////////
+
+////////////// task 1 ///////////////////
+
+// var names = [];
+// names[0] = "moiz"
+
+// console.log(names)
+
+//////////// task 3 ////////
+
+// var initialize = ["mioz" , "asad"]
+// console.log(initialize)
+
+//////////task 6 ////////
+
+// var initialize = ["mioz" , "asad" , 1 , 5 , "ebad"]
+// console.log(initialize)
+
+
+/////// task 7 ////////
+var education = ["SSC" , "HSC" , "BCS" , "BS" , "BCOM" , "MS" , "M. Phil" , "PhD"]
+
+document.write("Qualification"+ "<br>"+"1. "+education[0]+ "<br>"+"2. "+education[1]+ "<br>"+"3. "+education[2]+
+ "<br>"+"4. "+education[3]+ "<br>"+"5. "+education[4]+ "<br>"+"6. "+education[5]+ "<br>"+"7. "+education[6]+ "<br>"+"8. "+education[7])
+
+
+
+
+
+
+
+
+
+
+
+
